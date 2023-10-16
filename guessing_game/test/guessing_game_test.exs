e3ef07e8a754699d8d8fd31a5abd1536ed7f4f2e
Renan Medina <renan@silvamedina.com.br>
2023-10-16 15:21:55 -0300

    Using describe in test cases to group and improve test explanation

--- guessing_game/test/guessing_game_test.exs
+++ guessing_game/test/guessing_game_test.exs
@@ -3,30 +3,40 @@ defmodule GuessingGameTest do
 
   doctest GuessingGame
 
-  test "Target number is smaller than user guess" do
-    assert GuessingGame.check_guess(500, 250) == [:error, "The target number is smaller than 500"]
+  describe "When target number is smaller than user guess" do
+    test "returns :error atom with smaller text" do
+      assert GuessingGame.check_guess(500, 250) == [:error, "The target number is smaller than 500"]
+    end
   end
 
-  test "Target number is bigger than user guess" do
-    assert GuessingGame.check_guess(250, 546) == [:error, "The target number is bigger than 250"]
+  describe "When target number is bigger than user guess" do
+    test "returns :error atom with bigger text" do
+      assert GuessingGame.check_guess(250, 546) == [:error, "The target number is bigger than 250"]
+    end
   end
 
-  test "Target number is equal to user guess" do
-    assert GuessingGame.check_guess(120, 120) == :ok
+  describe "When target number is equal to user guess" do
+    test "returns :ok atom" do
+      assert GuessingGame.check_guess(120, 120) == :ok
+    end
   end
 
-  test "User still has available guesses: returns :continue atom" do
-    result = GuessingGame.take_guess(232, 500, 5)
-    assert result |> List.first == :continue
+  describe "When user still has available guesses" do
+    test "returns :continue atom" do
+      result = GuessingGame.take_guess(232, 500, 5)
+      assert result |> List.first == :continue
+    end
+
+    test "Decreases available guesses" do
+      result = GuessingGame.take_guess(232, 500, 5)
+      assert result |> List.last == 4
+    end
   end
 
-  test "User still has available guesses: should decrease available guesses" do
-    result = GuessingGame.take_guess(232, 500, 5)
-    assert result |> List.last == 4
-  end
-
-  test "User used all available guesses" do
-    result = GuessingGame.take_guess(232, 500, 1)
-    assert result |> List.first == :guesses_unavailable
+  describe "When user used all available guesses" do
+    test "returns :guesses_unavailable atom" do
+      result = GuessingGame.take_guess(232, 500, 1)
+      assert result |> List.first == :guesses_unavailable
+    end
   end
 end
