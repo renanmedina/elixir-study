d2d2f7848e54543f76f17e65e2d929b5e30adc11
Renan Medina <renan@silvamedina.com.br>
2023-10-15 23:11:53 -0300

    Trying out pattern matching for desconstruction

---
+++ list_and_pattern_matching.exs
@@ -0,0 +1,11 @@
+new_list = [1, 2, 3, 4, 5, 6]
+# pattern matching ('=' operator) extracting data from list
+[a, b, c, d, e, f] = new_list
+
+IO.puts("Variables matched from list: [a, b, c, d, e, f] = [1, 2, 3, 4, 5, 6]")
+IO.puts(a)
+IO.puts(b)
+IO.puts(c)
+IO.puts(d)
+IO.puts(e)
+IO.puts(f)
